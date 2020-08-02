fb272e02f6b442bbe79369e893e7a9097ad0381f
Arvind Maan <arvindmaan1199@gmail.com>
2020-08-01 20:59:32 -0500

    reworked the bet script

--- contracts/bet.sol
+++ contracts/bet.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.22 <0.7.0;
+pragma solidity ^0.6.6;
 
     
 contract Bet {
@@ -9,7 +9,7 @@ contract Bet {
     enum GameOutcome {PENDING, HOME, AWAY, DRAW}
     
     struct Game {
-        uint gameStart; //MM-DD-YY HH:MM
+        uint gameStart; //unix time
         Team home;
         Team away;
         GameOutcome outcome;
@@ -17,53 +17,48 @@ contract Bet {
     
     struct Participant {
         address payable walletId;
-        Game game;
-        Team teamSelected;
         uint amount;
     }
     
     Game game;
-    Participant bettor;
-    Participant taker;
+    Participant[] homers;
+    Participant[] awayers;
+    uint pool;
+
     
-    constructor(uint256 _timestamp, string memory _homeTeam, string memory _awayTeam, address payable _bettorWallet, address payable _takerWallet, bool _bettorHome, uint _bettorAmt, uint _takerAmt) public {
-        require (_bettorWallet.balance >= _bettorAmt);
-        require (_takerWallet.balance >= _takerAmt);
-        
-        game = Game(_timestamp, Team(_homeTeam), Team(_awayTeam), GameOutcome.PENDING);
-        bettor = Participant(_bettorWallet, game, _bettorHome ? Team(_homeTeam) : Team(_awayTeam), _bettorAmt);
-        taker = Participant(_takerWallet, game, _bettorHome ? Team(_awayTeam) : Team(_homeTeam), _takerAmt);
+    constructor(uint timestamp, string memory _homeTeam, string memory _awayTeam) public {
+        game = Game(timestamp, Team(_homeTeam), Team(_awayTeam), GameOutcome.PENDING);
     }
     
-    function setOutcome (uint _outcome) public {
+    function setOutcome(uint8 _outcome) public {
         require(game.outcome == GameOutcome.PENDING);
-        require(game.gameStart <= now);
-
-        game.outcome = GameOutcome(_outcome); 
+        require(game.gameStart < now);
         
-        bool payOutcome = payWinner();
-        if (payOutcome == false) {
-            pay(taker.walletId, taker.amount);    
-            pay(bettor.walletId, bettor.amount);  
-        }
+        game.outcome = GameOutcome(_outcome);
+        payWinners();
+        selfdestruct(msg.sender);
     }
     
-    function payWinner() public returns(bool){
-        require(game.outcome != GameOutcome.PENDING);
-        require(game.gameStart <= now);
+    function payWinners() public {
+        if (game.outcome == GameOutcome.HOME){
+            uint splitAmount = pool / homers.length;
+            for (uint i =0; i<homers.length; i++){
+                pay(homers[i].walletId, splitAmount);
+            }
+        } else if (game.outcome == GameOutcome.AWAY){
+            uint splitAmount = pool / awayers.length;
+            for (uint i =0; i<awayers.length; i++){
+                pay(awayers[i].walletId, splitAmount);
+            }
+        } else {
+            for (uint i =0; i<homers.length; i++){
+                pay(homers[i].walletId, homers[i].amount);
+            }
+            for (uint i =0; i<awayers.length; i++){
+                pay(awayers[i].walletId, awayers[i].amount);
+            }
+        }
 
-        Team memory winningTeam; 
-        if (game.outcome == GameOutcome.HOME)
-            winningTeam = game.home;
-        else if (game.outcome == GameOutcome.AWAY)
-            winningTeam = game.away;
-        else
-            return false;
-        
-        if (compareStrings(game.home.teamName, bettor.teamSelected.teamName))
-            return pay(bettor.walletId, bettor.amount + taker.amount);
-        else 
-            return pay(taker.walletId, bettor.amount + taker.amount);
     }
     
     function pay(address payable winner, uint amt) public payable returns(bool) {
@@ -71,14 +66,17 @@ contract Bet {
         winner.transfer(amt);
     }
     
-    function compareStrings (string memory a, string memory b) public pure returns (bool) {
-        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))) );
-    
-    }
-    
     event Received(address, uint);
     
-    receive() external payable {
+    function betOnHome() external payable {
+        homers.push(Participant(msg.sender, msg.value));
+        pool += msg.value;
+        emit Received(msg.sender, msg.value);
+    }
+    
+    function betOnAway() external payable {
+        awayers.push(Participant(msg.sender, msg.value));
+        pool += msg.value;
         emit Received(msg.sender, msg.value);
     }
 }
