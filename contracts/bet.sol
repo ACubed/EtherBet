fea6e74a8ffb1e296ce34bfd8e2adf24a4be54f0
Arvind Maan <arvindmaan1199@gmail.com>
2020-08-01 21:34:16 -0500

    added error messages to requires and minor linting changes

--- contracts/bet.sol
+++ contracts/bet.sol
@@ -1,79 +1,92 @@
 pragma solidity ^0.6.6;
 
-    
 contract Bet {
     struct Team {
         string teamName;
     }
-    
+
     enum GameOutcome {PENDING, HOME, AWAY, DRAW}
-    
+
     struct Game {
-        uint gameStart; //unix time
+        uint256 gameStart; //unix time
         Team home;
         Team away;
         GameOutcome outcome;
     }
-    
+
     struct Participant {
         address payable walletId;
-        uint amount;
+        uint256 amount;
     }
-    
+
     Game game;
     Participant[] homers;
     Participant[] awayers;
-    uint pool;
+    uint256 pool;
 
-    
-    constructor(uint timestamp, string memory _homeTeam, string memory _awayTeam) public {
-        game = Game(timestamp, Team(_homeTeam), Team(_awayTeam), GameOutcome.PENDING);
+    constructor(
+        uint256 timestamp,
+        string memory _homeTeam,
+        string memory _awayTeam
+    ) public {
+        game = Game(
+            timestamp,
+            Team(_homeTeam),
+            Team(_awayTeam),
+            GameOutcome.PENDING
+        );
     }
-    
+
     function setOutcome(uint8 _outcome) public {
-        require(game.outcome == GameOutcome.PENDING);
-        require(game.gameStart < now);
-        
+        require(game.outcome == GameOutcome.PENDING, "Game already concluded");
+        require(game.gameStart < now, "Game hasn't started yet");
+
         game.outcome = GameOutcome(_outcome);
         payWinners();
         selfdestruct(msg.sender);
     }
-    
+
     function payWinners() public {
-        if (game.outcome == GameOutcome.HOME){
-            uint splitAmount = pool / homers.length;
-            for (uint i =0; i<homers.length; i++){
+        if (game.outcome == GameOutcome.HOME) {
+            uint256 splitAmount = pool / homers.length;
+            for (uint256 i = 0; i < homers.length; i++) {
                 pay(homers[i].walletId, splitAmount);
             }
-        } else if (game.outcome == GameOutcome.AWAY){
-            uint splitAmount = pool / awayers.length;
-            for (uint i =0; i<awayers.length; i++){
+        } else if (game.outcome == GameOutcome.AWAY) {
+            uint256 splitAmount = pool / awayers.length;
+            for (uint256 i = 0; i < awayers.length; i++) {
                 pay(awayers[i].walletId, splitAmount);
             }
         } else {
-            for (uint i =0; i<homers.length; i++){
+            for (uint256 i = 0; i < homers.length; i++) {
                 pay(homers[i].walletId, homers[i].amount);
             }
-            for (uint i =0; i<awayers.length; i++){
+            for (uint256 i = 0; i < awayers.length; i++) {
                 pay(awayers[i].walletId, awayers[i].amount);
             }
         }
-
     }
-    
-    function pay(address payable winner, uint amt) public payable returns(bool) {
-        require(address(this).balance >= amt);
+
+    function pay(address payable winner, uint256 amt)
+        public
+        payable
+        returns (bool)
+    {
+        require(
+            address(this).balance >= amt,
+            "Not enough funds to pay the winner"
+        );
         winner.transfer(amt);
     }
-    
-    event Received(address, uint);
-    
+
+    event Received(address, uint256);
+
     function betOnHome() external payable {
         homers.push(Participant(msg.sender, msg.value));
         pool += msg.value;
         emit Received(msg.sender, msg.value);
     }
-    
+
     function betOnAway() external payable {
         awayers.push(Participant(msg.sender, msg.value));
         pool += msg.value;
