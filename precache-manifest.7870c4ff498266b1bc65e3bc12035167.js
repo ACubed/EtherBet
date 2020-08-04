6c4bc8228531447825a78dfe40827e2f4f7ac7b7
Arvind Maan <arvindmaan1199@gmail.com>
2020-08-04 14:35:11 -0500

    Updates

---
+++ precache-manifest.7870c4ff498266b1bc65e3bc12035167.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f19917c37f275e9f4e2f1aed8d859b91",
+    "url": "/EtherBet/index.html"
+  },
+  {
+    "revision": "df439ca84b0116312873",
+    "url": "/EtherBet/static/css/main.9d66b76c.chunk.css"
+  },
+  {
+    "revision": "e1e8e2fec3a82a11781a",
+    "url": "/EtherBet/static/js/2.53aeee9c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/EtherBet/static/js/2.53aeee9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df439ca84b0116312873",
+    "url": "/EtherBet/static/js/main.2b7b9f9e.chunk.js"
+  },
+  {
+    "revision": "66baae185cf6d4d793b9",
+    "url": "/EtherBet/static/js/runtime-main.3def1451.js"
+  },
+  {
+    "revision": "8c6389db4f63d4c9cf4a5899dc29ff23",
+    "url": "/EtherBet/static/media/EB.8c6389db.png"
+  },
+  {
+    "revision": "16deff09447ada400b78c6a5278a17d0",
+    "url": "/EtherBet/static/media/LCS_2020_Logo.16deff09.png"
+  },
+  {
+    "revision": "2febb4f6a7f8b7e5842959f6310c7d09",
+    "url": "/EtherBet/static/media/NBA_2020_Logo.2febb4f6.png"
+  },
+  {
+    "revision": "4c18a9012a741d02967db19b2f3abf5c",
+    "url": "/EtherBet/static/media/NHL_2020_Logo.4c18a901.png"
+  }
+]);
