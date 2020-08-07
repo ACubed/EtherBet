07d27c68bc9ce50a316485adb0051d34778a80c3
Arvind Maan <arvindmaan1199@gmail.com>
2020-08-07 17:34:40 -0500

    changed to api to return json
    Co-Authored-By: Andy Tan <33612287+andy-tan7@users.noreply.github.com>
    Co-Authored-By: Caden Chabot <chabotc@myumanitoba.ca>

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+    return res.status(200).json({ title: 'Express' });
 });
 
 module.exports = router;
