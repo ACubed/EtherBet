952c655b76bc84536bdd3d3df5d2c492b54e0916
Arvind Maan <arvindmaan1199@gmail.com>
2020-08-07 19:38:45 -0500

    Update app.js
    Co-Authored-By: Andy Tan <33612287+andy-tan7@users.noreply.github.com>
    Co-Authored-By: Caden Chabot <chabotc@myumanitoba.ca>

--- api/src/app.js
+++ api/src/app.js
@@ -8,6 +8,7 @@ var app = express();
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017', {
+    useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 const db = mongoose.connection;
